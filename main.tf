adb5eaf0d4031fc9353ace31a7f3f6a5a6eee12c
Shubham Thakur <shubham.thakur@gmail.com>
2022-07-05 01:56:05 +0000

    hi

--- main.tf
+++ main.tf
@@ -15,7 +15,7 @@ provider "aws" {
 }
 
 resource "aws_s3_bucket" "b" {
-  bucket = "my-tf-test-bucket-sl-shubham00124515"
+  bucket = "my-tf-test-bucket-sl-shubham-001234"
 
   tags = {
     Name        = "My bucket"
